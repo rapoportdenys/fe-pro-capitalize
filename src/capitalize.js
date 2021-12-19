68fafb778e51e7d1865cfbe9a7f944f764d9c189
Denys <rapoportden@gmail.com>
2021-12-19 13:52:13 +0200

    add function capitalize

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,11 @@
 function capitalize(string) {
+    let result = string.split(' ');
+
+    let newArray = result.map(function (item) {
+        return item.charAt(0).toUpperCase() + item.slice(1);
+    });
+   
+    return newArray.join(' ');
 }
 
 module.exports = capitalize;
